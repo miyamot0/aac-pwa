9d8f1fe1938ad74dce77c13f0bbe36a39ab50164
Shawn Gilroy <shawnpgilroy@gmail.com>
2024-10-16 09:17:01 -0500

    Trim out dev options

--- vite.config.ts
+++ vite.config.ts
@@ -5,30 +5,34 @@ import { VitePWA } from 'vite-plugin-pwa'
 import manifest from './public/manifest.json'
 import package_json from './package.json'
 
-export default defineConfig({
-    plugins: [
-        react(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            workbox: {
-                globPatterns: ['**/*'],
-                cleanupOutdatedCaches: true,
-                sourcemap: false
-            },
-            devOptions: {
-                enabled: true
-            },
-            // @ts-expect-error - VitePWA types are incorrect
-            manifest: { ...manifest }
-        })
-    ],
-    define: {
-        BUILD_DATE: JSON.stringify(new Date().toLocaleDateString('en-US')),
-        BUILD_VERSION: JSON.stringify(package_json.version)
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+    const is_development = mode === 'serve'
+
+    return {
+        plugins: [
+            react(),
+            VitePWA({
+                registerType: 'autoUpdate',
+                workbox: {
+                    globPatterns: ['**/*'],
+                    cleanupOutdatedCaches: true,
+                    sourcemap: false
+                },
+                devOptions: {
+                    enabled: is_development
+                },
+                // @ts-expect-error - VitePWA types are incorrect
+                manifest: { ...manifest }
+            })
+        ],
+        define: {
+            BUILD_DATE: JSON.stringify(new Date().toLocaleDateString('en-US')),
+            BUILD_VERSION: JSON.stringify(package_json.version)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
         }
     }
 })
